5e066d6e2294c6813464d953db479504147f014b
AkshitaSharma211 <sharma.1608akshita@gmail.com>
2026-09-23 15:40:51 +0530

    Add Problem and Submission schemas

---
+++ backend/models/Problem.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const testCaseSchema = new mongoose.Schema({
+  input: { type: String, required: true },
+  output: { type: String, required: true },
+});
+
+const problemSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  slug: { type: String, required: true, unique: true },
+  description: { type: String, required: true },
+  difficulty: {
+    type: String,
+    enum: ["Easy", "Medium", "Hard"],
+    required: true,
+  },
+  sampleTestCases: [testCaseSchema],
+  hiddenTestCases: [testCaseSchema],
+}, { timestamps: true });
+
+module.exports = mongoose.model("Problem", problemSchema);
